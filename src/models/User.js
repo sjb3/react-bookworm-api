c5d600f823084ff780e6b5f876a8e86fafe856bc
Sung J Byun <sungjbyun@D-173-250-185-187.dhcp4.washington.edu>
2017-09-17 21:06:03 -0700

    adding jwt and stuff

---
+++ src/models/User.js
@@ -0,0 +1,31 @@
+
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+// TODO: add uniqueness and email verigication to E mail
+
+const schema = new mongoose.Schema({
+  email: { type: String, required: true, lowercase: true, index: true },
+  passwordHash: { type: String, required: true}
+  }, { timestamp: true }
+);
+
+schema.methods.isValidPassword = function isValidPassword(password) {
+  return bcrypt compareSync(password, this.passwordHash);
+};
+
+schema.methods.generateJWT = function generateJWT() {
+  return jwt.sign({
+    email: this.email
+  }, process.env.JWT_SECRET)
+}
+
+schema.methods.toAuthJSON = function toAuthJSON() {
+  return {
+    emaiol: this.email,
+    token: this.generateJWT()
+  }
+}
+
+export default mongoose.model('User', schema);
